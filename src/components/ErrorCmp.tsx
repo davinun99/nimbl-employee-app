cfeffeef8e1706ade94786cf24a29b3ea930193e
David <nundavid99@gmail.com>
2023-01-18 10:44:01 -0300

    Fix error cmp

--- src/components/ErrorCmp.tsx
+++ src/components/ErrorCmp.tsx
@@ -1,14 +1,22 @@
 type Props = {
 	error: any;
 }
-const ErrorCmp = ({error}: Props) => (
-	<div>
-		<p>Error: {error.toString()}</p>
-		{error?.message && <p>Message: {error.message}</p>}
-		{error?.response?.message && <p>Response message: {error.response.message}</p>}
-		{error?.response?.data && <p>Response data: {error.response.data}</p>}
-		{/* <p>Response data: {error?.response?.data}</p> */}
-	</div>
-);
+const ErrorCmp = ({error}: Props) => {
+	let data = "";
+	let dataMsg = "";
+	try {
+		data = JSON.stringify(error?.response?.data);
+		dataMsg = error?.response?.data[0]?.message;
+	} catch (error) {}
+	return (
+		<div>
+			{/* <p>Error: {error?.toString()}</p> */}
+			{error?.message && <p>Message: {error.message}</p>}
+			{error?.response?.message && <p>Response message: {error.response.message}</p>}
+			{data && <p>Response data message: {dataMsg}</p>}
+			{data && <p>Response data: {data}</p>}
+		</div>
+	);
+};
 
 export default ErrorCmp;
