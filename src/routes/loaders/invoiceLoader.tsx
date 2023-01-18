a42ca952e2fadf5781aac9ea4f84cbd81d797f48
David <nundavid99@gmail.com>
2023-01-18 10:44:32 -0300

    invoice loader using utils

--- src/routes/loaders/invoiceLoader.tsx
+++ src/routes/loaders/invoiceLoader.tsx
@@ -1,44 +1,25 @@
-import { Expense, Invoice, NimblUser } from '../../types';
+import { Invoice, NimblUser } from '../../types';
 import { ActionFunctionArgs, defer, redirect } from "react-router-dom";
 import { getNimblUser } from "../../utils/localStorage";
-import axiosClient from '../../utils/axios';
-import { handleError } from '../../utils/errorUtils';
+import { getListFromEndpoint, putDataToEndpoint } from '../../utils/axios';
 
-const invoicePromise = async () => {
-	try {
-		const resp = await axiosClient.get<Expense[]>(`/employeemonths`);
-		return resp?.data || [];
-	} catch (error: any) {
-		handleError(error, "Error getting your invoices");
-		return [];	
-	}
-}
 const invoiceLoader = async () => {
 	const nimblUser = getNimblUser();
 	if(!nimblUser){
 		return redirect('/login');
 	}
-	let invoices = invoicePromise();
-	let error = '';
-	return defer({ nimblUser, error, invoices });
+	let invoices = getListFromEndpoint<Invoice>('/employeemonths', 'Error getting your invoices');
+	return defer({ nimblUser, invoices });
 }
-const updateInvoicePromise = async(formData: FormData) => {
-	const id = formData.get('id');
-	formData.delete('id');
-	try {
-		const options = { headers:{ 'Content-Type': 'multipart/form-data' } };
-        const req = await axiosClient.put(`/employeemonth/${id}/invoice`, formData, options );
-		return req.data;
-	} catch (error) {
-		handleError(error, "Error saving your document");
-		return null;
-	}
-}
+
 export default invoiceLoader;
 
 export const editInvoiceAction = async ({ request, params }: ActionFunctionArgs) => {
 	const formData = await request.formData();
-	return updateInvoicePromise(formData);
+	const id = formData.get('id');
+	formData.delete('id');
+	const options = { headers:{ 'Content-Type': 'multipart/form-data' } };
+	return putDataToEndpoint(`/employeemonth/${id}/invoice`, formData, 'Error saving your document' ,options);
 }
 
 export type InvoiceLoader = {
