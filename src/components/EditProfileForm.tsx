cd5e86b145b231aba1c6ee3653504d2fc2596d3c
David <nundavid99@gmail.com>
2023-01-19 11:25:54 -0300

    Edit profile form

--- src/components/EditProfileForm.tsx
+++ src/components/EditProfileForm.tsx
@@ -1,17 +1,15 @@
 import { useState } from "react";
-import { Form, useFetcher } from "react-router-dom";
-import { Button, Col, FormGroup, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader, Row } from "reactstrap";
+import { useFetcher } from "react-router-dom";
+import { Button, Card, CardBody, Col, FormGroup, Input, Label, Row } from "reactstrap";
 import { NimblUser } from "../types";
 import LoaderCmp from "./LoaderCmp";
 
 type Props = {
-	isOpen: boolean;
 	recruiter: NimblUser;
-	toggleModal: () => void;
 };
 
-const EditRecruiterModal = ({
-	isOpen, recruiter, toggleModal
+const EditProfileForm = ({
+	recruiter
 }: Props) => {
 	const [editedRecruiter, setEditedRecruiter] = useState(recruiter);
 	const fetcher = useFetcher();
@@ -21,51 +19,45 @@ const EditRecruiterModal = ({
 			[e.currentTarget.name]: e.currentTarget.value,
 		});
 	};
-	const handleFinishedEdit = () => {
-		setTimeout( () => {
-			toggleModal();
-		}, 2000);
-	}
 	return (
-		<Modal isOpen={isOpen} toggle={toggleModal}>
-			{fetcher.state !== 'idle' && <LoaderCmp />}
-			<ModalHeader toggle={toggleModal}>Edit recruiter</ModalHeader>
-			<fetcher.Form method='put' onSubmit={handleFinishedEdit}>
-				<ModalBody>
+		<Card>
+			<CardBody>
+				{fetcher.state !== 'idle' && <LoaderCmp />}
+				<fetcher.Form method='put'>
 					<input name="id" value={recruiter.recruiter_id} type="hidden" />
 					<Row>
-						<Col sm={12} lg={6}>
+						<Col sm={12} lg={4}>
 							<FormGroup>
 								<Label for="recruiter_first_name">First name</Label>
 								<Input required name="recruiter_first_name" id="recruiter_first_name" onChange={handleChange} value={editedRecruiter.recruiter_first_name} />
 							</FormGroup>
 						</Col>
-						<Col sm={12} lg={6}>
+						<Col sm={12} lg={4}>
 							<FormGroup>
 								<Label for="recruiter_last_name">Last name</Label>
 								<Input required name="recruiter_last_name" id="recruiter_last_name" onChange={handleChange} value={editedRecruiter.recruiter_last_name} />
 							</FormGroup>
 						</Col>
-						<Col sm={12} lg={6}>
+					</Row>
+					<Row>
+						<Col sm={12} lg={4}>
 							<FormGroup>
 								<Label for="recruiter_email">Email</Label>
 								<Input type="email" required name="recruiter_email" id="recruiter_email" onChange={handleChange} value={editedRecruiter.recruiter_email} />
 							</FormGroup>
 						</Col>
-						<Col sm={12} lg={6}>
+						<Col sm={12} lg={4}>
 							<FormGroup>
 								<Label for="city">City</Label>
 								<Input required name="city" id="city" onChange={handleChange} value={editedRecruiter.city} />
 							</FormGroup>
 						</Col>
 					</Row>
-				</ModalBody>
-				<ModalFooter>
 					<Button type="submit">Save changes</Button>
-				</ModalFooter>
-			</fetcher.Form>
-		</Modal>
+				</fetcher.Form>
+			</CardBody>
+		</Card>
 	)
 }
 
-export default EditRecruiterModal;
+export default EditProfileForm;
