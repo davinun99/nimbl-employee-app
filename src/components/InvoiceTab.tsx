87ed127aaf7dee6b31f09cab53da91ed6e2d08de
David <nundavid99@gmail.com>
2023-01-26 13:29:35 -0300

    Fix invoice tab with last invoice

--- src/components/InvoiceTab.tsx
+++ src/components/InvoiceTab.tsx
@@ -6,19 +6,32 @@ import { getDateFromStr } from '../utils/dateUtils';
 type Props = {
 	invoices: Invoice[];
 };
+type LastInvoiceCmpProps = {
+	invoice?: Invoice | null;
+};
+const LastInvoiceCmp = ({ invoice }: LastInvoiceCmpProps) => {
+	if(!invoice){
+		return <></>;
+	}
+	const dt = getDateFromStr(invoice.date);
+	let lastInvoiceTitle = dt?.toLocaleString('en-us', { month: 'long' });
+	lastInvoiceTitle = lastInvoiceTitle ? `${lastInvoiceTitle} ${dt?.getFullYear()}` : 'Last';
+	
+	return (
+		!invoice ? <></>:
+		<main>
+			<h6 className="font-weight-bold">{lastInvoiceTitle} invoice:</h6>
+			<UploadInvoiceForm invoice={invoice} col={6} />
+		</main>
+	);
+}
 const InvoiceTab = ({ invoices }: Props) => {
 	invoices.sort((i1, i2) => i1.date > i2.date ? -1 : 1);
 	const lastInvoice = invoices[0];
-	const dt = getDateFromStr(lastInvoice.date);
-	let lastInvoiceTitle = dt?.toLocaleString('en-us', { month: 'long' });
-	lastInvoiceTitle = lastInvoiceTitle ? `${lastInvoiceTitle} ${dt?.getFullYear()}` : 'Last';
 	const restInvoices = invoices.slice(1);
 	return (
 		<>
-			<main>
-				<h6 className="font-weight-bold">{lastInvoiceTitle} invoice:</h6>
-				<UploadInvoiceForm invoice={lastInvoice} col={6} />
-			</main>
+			<LastInvoiceCmp invoice={lastInvoice}/>
 			<InvoicesGrid invoices={restInvoices} />
 		</>
 	)
